a4f9a6ece443fc69bfd05506c92946930bab2d7d
in-deepaknegi <negideepak.in@gmail.com>
2023-12-16 19:06:28 +0530

    updated responsiveness

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
                 className='fixed inset-0 z-[-10] h-full w-full object-cover brightness-110'
             />
             <div className='absolute w-full h-full z-[-9] bg-gradient-to-b from-black/10 via-black/60 to-black'></div>
-            <div className='mx-auto max-w-7xl px-6 lg:px-8 py-24 lg:py-28'>
+            <div className='mx-auto max-w-7xl px-6 lg:px-8 py-32 lg:py-28'>
                 <div className="hidden max-w-4xl sm:mb-8 sm:flex">
                     <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-200 ring-1 ring-gray-100 hover:ring-gray-100">
                         Announcing our next round of funding... <a href="#" className="font-semibold text-primary"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
@@ -33,8 +33,7 @@ const Hero = () => {
                     </h1>
                 )}
 
-                <div className='ml-auto max-w-xl mt-12'>
-
+                <div className='mx-auto max-w-xl mt-12 lg:ml-auto lg:mx-0'>
                     {lang === 'en' ? (
                         <p className='text-left text-lg text-white font-light md:text-xl'>
                             On an endless journey to create experiences that inspire others. Always motived by design that&apos;s honest, aesthetic and natural. Probably the only designer you&apos;ll ever need.
@@ -43,9 +42,7 @@ const Hero = () => {
                         <p className='text-left tracking-widest text-lg text-white font-light md:text-xl'>
                             दूसरों को प्रेरित करने वाले अनुभव बनाने की अंतहीन यात्रा पर। हमेशा ऐसे डिज़ाइन से प्रेरित होता है जो ईमानदार, सौंदर्यपूर्ण और प्राकृतिक हो। संभवतः एकमात्र डिज़ाइनर जिसकी आपको कभी आवश्यकता होगी। दूसरों को प्रेरित करने वाले अनुभव बनाने की एक अंतहीन यात्रा पर। हमेशा ऐसे डिज़ाइन से प्रेरित होता है जो ईमानदार, सौंदर्यपूर्ण और प्राकृतिक हो। संभवतः एकमात्र डिज़ाइनर जिसकी आपको कभी आवश्यकता होगी।
                         </p>
-
                     )}
-
 
                     <div className="mt-10 flex gap-x-6">
                         <a href="#" className="rounded-2xl bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
