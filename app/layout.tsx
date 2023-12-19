5d000d16b80c04fe4e6a65376e1b0ac563799be4
in-deepaknegi <negideepak.in@gmail.com>
2023-12-20 00:08:18 +0530

    updated site-name for google search --test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head';
 import { LanguageProvider } from '@/library/LangChange';
 import { CartProvider } from '@/library/CartContext';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Mridakriti',
   description: 'Mridakriti',
 }
@@ -16,8 +17,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Mridakrit",
+    "url": "https://www.mridakriti.com"
+    // Add more organization data as needed
+  };
+
   return (
     <html lang="en">
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+      </Head>
       < CartProvider >
         <LanguageProvider>
           <body className={inter.className}>
