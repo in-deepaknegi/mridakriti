8adb3c34d0aad328cce62a2c21a98c9c3bf6f91d
negideepak.in@gmail.com <negideepak.in@gmail.com>
2024-03-16 10:36:46 +0530

    misc

--- app/paintings/page.tsx
+++ app/paintings/page.tsx
@@ -17,7 +17,7 @@ const page = async () => {
         <>
             <Navbar />
             <main>
-                <section className="relative bg-dusk-100/30 isolate overflow-hidden">
+                <section className="relative bg-dusk-1t00/30 isolate overflow-hidden">
                     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
                             {" "}
